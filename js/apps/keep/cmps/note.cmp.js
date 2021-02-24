95c800364481d90ba9dab7590ea84b69973190d7
SQUID13 <SQUADINIO13@gmail.com>
2021-02-24 10:47:42 +0200

    added basic css folder layout

---
+++ js/apps/keep/cmps/note.cmp.js
@@ -0,0 +1,11 @@
+
+export default {
+    props: ['txt'],
+    template: `
+    <div class="note">
+        <h1>Note</h1>
+        <p>{{txt}}</p>
+    </div>
+    `,
+
+}
--- js/apps/keep/pages/keep.page.cmp.js
+++ js/apps/keep/pages/keep.page.cmp.js
@@ -1,8 +1,20 @@
 
+import note from '../cmps/note.cmp.js'
+
 export default {
     template: `
-    <section class="mail-app">
+    <section class="keep-app">
         <h1>keepApp</h1>
+        <note :txt="'hello'"> </note>
+        <router-link to="/"> Home </router-link>
     </section>
-    `
+    `,
+    data() {
+        return {
+
+        }
+    },
+    components: {
+        note
+    }
 }
