f6e8dfe0b76c2be3f5989a2834e6230adfae075e
HadarHarush <hadarushha@gmail.com>
2021-02-24 15:16:33 +0200

    jklkj

--- js/apps/mail/cmps/mail-card.cmp.js
+++ js/apps/mail/cmps/mail-card.cmp.js
@@ -2,9 +2,9 @@ export default {
   props: ['mail'],
   template: `
         <article class="mail-card center">
-            <p class="mail-sender">from: {{mail.addresses.from}}</p>
-            <p class="mail-time">timestamp: {{mail.general.timestamp}}</p>
-            <p class="mail-subject">subject: {{mail.content.subject}}</p>
+            <p class="mail-sender"><span class="bold">from:</span> {{mail.addresses.from}}</p>
+            <p class="mail-time"><span class="bold">timeStamp:</span> {{mail.general.timestamp}}</p>
+            <p class="mail-subject"><span class="bold">subject:</span> {{mail.content.subject}}</p>
         </article>
     `,
   computed: {},
--- js/apps/mail/cmps/mail-list.cmp.js
+++ js/apps/mail/cmps/mail-list.cmp.js
@@ -3,7 +3,7 @@ import mailCard from './mail-card.cmp.js'
 export default {
   props: ['mails'],
   template: `
-        <section class="mail-list flex column">
+        <section class="mail-list flex column flex-1">
             <mail-card v-if="mails.length" v-for="mail in mails" :key="mail.id" :mail="mail"/>
         </section>
     `,
