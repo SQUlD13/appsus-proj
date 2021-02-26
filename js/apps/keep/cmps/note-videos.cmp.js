00b4d2d49256e55e57d9712cc3922deb3fdc8165
SQUID13 <SQUADINIO13@gmail.com>
2021-02-26 14:39:21 +0200

    youtube width adjustment implemented

--- js/apps/keep/cmps/note-videos.cmp.js
+++ js/apps/keep/cmps/note-videos.cmp.js
@@ -4,16 +4,32 @@ import deleteBtn from '../../../cmps/delete-btn.cmp.js'
 export default {
     props: ['videos'],
     template: `
-        <div class="note-videos">
-                <template v-if="videos">
+        <div v-if="videos" class="note-videos">
                     <template v-for="(video,idx) in videos">
-                        <!-- <pre>{{video}}</pre> -->
-                        <iframe :src="video.url" width="500px" height="250px"></iframe>
+                        <iframe :ref="idx" :src="video.url" :width="width" height="250px"></iframe>
                         <delete-btn  @delete="$emit('delete-vid',video.id)" />
                     </template>
-                </template>
             </div>
     `,
+    data() {
+        return {
+            width: null,
+        }
+    },
+    mounted() {
+        var refs = this.$refs
+        if (refs[0]) {
+            var vidEl = Object.values(refs)[0]
+            var parentW = vidEl[0].parentElement.offsetWidth
+            this.width = parentW + 'px'
+            vidEl.width = this.width
+        }
+    },
+    watch: {
+        width(newVal, oldVal) {
+            this.width = newVal
+        }
+    },
     components: { deleteBtn }
 
 }
